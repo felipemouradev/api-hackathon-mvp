4c305da532bbff2ef93fb64dcbcb6410295e86c0
Guilherme Gehres <guilhermegehres95@gmail.com>
2019-10-19 19:12:45 -0300

    itinerarios

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,15 @@
-import {Controller, Get, Header, Param, Req, Res} from '@nestjs/common';
+import {Controller, Get, Header, Param, Req, Res, Query} from '@nestjs/common';
 import {AppService} from './app.service';
-
+import * as json from './itinerarios.json';
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {
+    }
+
+    @Get('itinerarios')
+    getItinerario(
+        @Query('itinerario') itinerario: string,
+    ) {
+        return json[itinerario];
     }
 }
